0673a9f9b16877278b6be551d86989adddb55ab3
BCD2018 <15011700342Xuan>
2020-10-20 21:37:51 -0500

    readme

--- run_mtcl.py
+++ run_mtcl.py
@@ -40,7 +40,7 @@ elif args.experiment=='mixceleba':
     from dataloaders import mixceleba as dataloader
 
 # Args -- Approach
-if 'mtcl_ncl' in args.approach:
+if 'mtcl_ncl' in args.approach: #MTCL refers to CAT
     from approaches import mtcl_ncl as approach
 
 if 'mlp_mtcl' in args.approach:
